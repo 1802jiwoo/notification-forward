0292e199665d940f0ab93bc82ff64c82f3ec5629
musi1802 <2008jiwoo@gmail.com>
2026-09-08 09:16:12 +0900

    feat: 슬랙 채널 추가 ui

--- lib/screens/channel/add_slack_channel_screen.dart
+++ lib/screens/channel/add_slack_channel_screen.dart
@@ -1,12 +1,118 @@
 import 'package:flutter/material.dart';
 
-class AddSlackChannelScreen extends StatelessWidget {
+import '../../core/colors.dart';
+import '../../core/text_styles.dart';
+import '../../widgets/back_icon_button.dart';
+import '../../widgets/custom_long_text_button.dart';
+import '../../widgets/form_field_row.dart';
+
+class AddSlackChannelScreen extends StatefulWidget {
   const AddSlackChannelScreen({super.key});
+
+  @override
+  State<AddSlackChannelScreen> createState() => _AddSlackChannelScreenState();
+}
+
+class _AddSlackChannelScreenState extends State<AddSlackChannelScreen> {
+  final TextEditingController nameController = TextEditingController();
+  final TextEditingController webhookURLController = TextEditingController();
+  final TextEditingController channelController = TextEditingController();
+
+  @override
+  void dispose() {
+    nameController.dispose();
+    webhookURLController.dispose();
+    channelController.dispose();
+    super.dispose();
+  }
+
+  Future<void> saveChannel() async {}
+
+  Future<void> sendTest() async {}
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(child: Column()),
+      appBar: AppBar(
+        titleSpacing: -5,
+        title: Text('슬랙 채널', style: textStyleW600(fontSize: 18)),
+        leading: const BackIconButton(),
+      ),
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20),
+          child: Stack(
+            children: [
+              Positioned.fill(child: _slackChannelForm()),
+              Align(
+                alignment: Alignment.bottomCenter,
+                child: Row(
+                  spacing: 15,
+                  children: [
+                    Expanded(
+                      child: CustomLongTextButton(
+                        title: '테스트 전송',
+                        callback: sendTest,
+                        isFill: false,
+                      ),
+                    ),
+                    Expanded(
+                      child: CustomLongTextButton(
+                        title: '저장',
+                        callback: saveChannel,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
+
+  Widget _slackChannelForm() => SingleChildScrollView(
+    child: Column(
+      children: [
+        FormFieldRow(
+          title: '이름',
+          child: TextField(
+            controller: nameController,
+            decoration: InputDecoration(
+              isCollapsed: true,
+              border: InputBorder.none,
+              hintText: '채널 이름을 입력하세요',
+              hintStyle: textStyleW500(color: textHint, fontSize: 15),
+            ),
+          ),
+        ),
+        FormFieldRow(
+          title: '웹훅 URL',
+          child: TextField(
+            controller: webhookURLController,
+            maxLines: null,
+            decoration: InputDecoration(
+              isCollapsed: true,
+              border: InputBorder.none,
+              hintText: '웹훅 주소를 입력해주세요',
+              hintStyle: textStyleW500(color: textHint, fontSize: 15),
+            ),
+          ),
+        ),
+        FormFieldRow(
+          title: '채널',
+          child: TextField(
+            controller: channelController,
+            decoration: InputDecoration(
+              isCollapsed: true,
+              border: InputBorder.none,
+              hintText: '채널을 입력해주세요',
+              hintStyle: textStyleW500(color: textHint, fontSize: 15),
+            ),
+          ),
+        ),
+      ],
+    ),
+  );
 }

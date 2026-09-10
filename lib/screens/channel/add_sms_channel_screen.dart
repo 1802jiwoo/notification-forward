1bea23fe45d83e37e4894cd92b4dfc849e9c6454
musi1802 <2008jiwoo@gmail.com>
2026-09-10 15:19:35 +0900

    feat: SMS 권한 요청

--- lib/screens/channel/add_sms_channel_screen.dart
+++ lib/screens/channel/add_sms_channel_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:permission_handler/permission_handler.dart';
 
 import '../../core/colors.dart';
 import '../../core/text_styles.dart';
@@ -17,6 +18,16 @@ class _AddSmsChannelScreenState extends State<AddSmsChannelScreen> {
   final TextEditingController nameController = TextEditingController();
   final TextEditingController webhookURLController = TextEditingController();
   final TextEditingController channelController = TextEditingController();
+  bool isSmsPermissionGranted = false;
+
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.addPostFrameCallback((timeStamp) async {
+      isSmsPermissionGranted = await Permission.sms.status.isGranted;
+      setState(() {});
+    });
+  }
 
   @override
   void dispose() {
@@ -29,6 +40,14 @@ class _AddSmsChannelScreenState extends State<AddSmsChannelScreen> {
   Future<void> saveChannel() async {}
 
   Future<void> sendTest() async {}
+
+  Future<void> requestSmsPermission() async {
+    final status = await Permission.sms.request();
+
+    setState(() {
+      isSmsPermissionGranted = status.isGranted;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -43,12 +62,13 @@ class _AddSmsChannelScreenState extends State<AddSmsChannelScreen> {
           padding: const EdgeInsets.symmetric(horizontal: 20),
           child: Stack(
             children: [
-              Positioned.fill(child: SingleChildScrollView(child: Column(
-                children: [
-                  const _WarningMessage(),
-                  _smsChannelForm()
-                ],
-              ))),
+              Positioned.fill(
+                child: SingleChildScrollView(
+                  child: Column(
+                    children: [const _WarningMessage(), _smsChannelForm()],
+                  ),
+                ),
+              ),
               Align(
                 alignment: Alignment.bottomCenter,
                 child: Row(
@@ -107,7 +127,10 @@ class _AddSmsChannelScreenState extends State<AddSmsChannelScreen> {
         title: '문자 권한',
         child: Row(
           children: [
-            Text('문자 권한 확인', style: textStyleW500(color: Colors.red),),
+            Text(
+              isSmsPermissionGranted ? '문자 권한이 허용되어 있습니다.' : '문자 권한을 허용해주세요.',
+              style: textStyleW500(color: isSmsPermissionGranted ? textPrimary : Colors.red),
+            ),
             const Spacer(),
             TextButton(
               style: TextButton.styleFrom(
@@ -116,13 +139,8 @@ class _AddSmsChannelScreenState extends State<AddSmsChannelScreen> {
                 tapTargetSize: MaterialTapTargetSize.shrinkWrap,
                 overlayColor: Colors.transparent,
               ),
-              onPressed: () {
-
-              },
-              child: Text(
-                '권한 요청',
-                style: textStyleW700(color: primary),
-              ),
+              onPressed: requestSmsPermission,
+              child: Text('권한 요청', style: textStyleW700(color: primary)),
             ),
           ],
         ),
@@ -155,10 +173,7 @@ class _WarningMessage extends StatelessWidget {
               color: warning,
             ),
             child: Center(
-              child: Text(
-                '!',
-                style: textStyleW900(color: Colors.white),
-              ),
+              child: Text('!', style: textStyleW900(color: Colors.white)),
             ),
           ),
           Expanded(
@@ -166,14 +181,14 @@ class _WarningMessage extends StatelessWidget {
               spacing: 2,
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                Text('문자 발송 요금이 부과될 수 있어요', style: textStyleW700(
-                  color: onWarningContainer,
-                  fontSize: 14,
-                ),),
-                Text('기기의 문자 기능으로 보내므로 통신사 요금제가 적용됩니다.', style: textStyleW400(
-                  fontSize: 12,
-                  color: onWarningContainer,
-                ),),
+                Text(
+                  '문자 발송 요금이 부과될 수 있어요',
+                  style: textStyleW700(color: onWarningContainer, fontSize: 14),
+                ),
+                Text(
+                  '기기의 문자 기능으로 보내므로 통신사 요금제가 적용됩니다.',
+                  style: textStyleW400(fontSize: 12, color: onWarningContainer),
+                ),
               ],
             ),
           ),
@@ -182,4 +197,3 @@ class _WarningMessage extends StatelessWidget {
     );
   }
 }
-
